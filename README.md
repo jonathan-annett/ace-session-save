0001caa77fc2680347a2b9bae7588ea093905192
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-19 21:53:40 +1100

    first commit (ish)

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ace-session-save
+
+**installation**
+
+    npm i github:jonathan-annett/ace-session-save
+    
+
+**installation - demo mode**
+
+(installs a few extra dependancies for node.js launching via express)
+
+    mkdir ace-session-save-demo
+    cd ace-session-save-demo
+    echo {} > ./package.json
+    npm i github:jonathan-annett/ace-session-save --save
+    cd node_modules/ace-session-save
+    npm test
+
+**usage**
+
+
+install demo mode (see above) or just look at dist/index.html
+
+    
--- index.js
+++ index.js
@@ -1,91 +1,110 @@
+require("./@")(module,function(window) {
+    
+    
+    var filterHistory = function(deltas){ 
+        return deltas.filter(function (d) {
+            return d.group != "fold";
+        });
+    }
+    
+    var sessionToJSON = function(session) {
+        return {
+            selection: session.selection.toJSON(),
+            value: session.getValue(),
+            history: {
+                undo: session.$undoManager.$undoStack.map(filterHistory),
+                redo: session.$undoManager.$redoStack.map(filterHistory)
+            },
+            scrollTop: session.getScrollTop(),
+            scrollLeft: session.getScrollLeft(),
+            options: session.getOptions()
+        }
+    }
+    
+    var sessionFromJSON = function(data) {
+        var session = ace.createEditSession(data.value);
+        session.$undoManager.$doc = session; // workaround for a bug in ace
+        session.setOptions(data.options);
+        session.$undoManager.$undoStack = data.history.undo;
+        session.$undoManager.$redoStack = data.history.redo;
+        session.selection.fromJSON(data.selection);
+        session.setScrollTop(data.scrollTop);
+        session.setScrollLeft(data.scrollLeft);
+        return session;
+    };
+    
+    function serialize (editor,omit,cb) {
+        if (typeof omit==='function') {
+            cb=omit;
+            omit=undefined;
+        }
+        try {
+             cb(undefined,(function(session){
+                 
+            
+                var object = {
+                    editor : {
+                        theme : editor.getTheme().split("/").pop()
+                    },
+                    session : sessionToJSON(session)
+                }
+                
+                if (omit) {
+                    omit.forEach(function(k){delete object.session[k];delete object.editor[k]});
+                }
+            
+                return JSON.stringify(object);
+                
+            })(editor.getSession()));
+        } catch (err) {
+            cb(err);
+        }
+    }
+    
+    function deserialize (editor,json,cb) {
+        
+        var Range = window.ace.require("ace/range").Range;
+        
+        function deltaRangeFixup (delta) {
+            delta.range = Range.fromPoints(delta.range.start, delta.range.end);
+        }
+        
+        function deltaArrayFixup(element) {
+            element.deltas.forEach(deltaRangeFixup);
+        }
+        
+        function stackElementFixup(stackElement) {
+            stackElement.forEach(deltaArrayFixup);
+        }
+        
+        function stackArrayFixup(stackArray) {
+            if (!stackArray) return [];
+            stackArray.forEach(stackElementFixup);
+            return stackArray;
+        }
+        
 
-function serialize (editor,cb) {
-	try {
-		 cb(undefined,(function(session){
-			
-			return JSON.stringify({
-				editor : {
-					theme : editor.getTheme().split("/").pop()
-				},
-				session : {
-					value : session.getValue(),
-					selection : session.selection.toJSON(),
-					options   : session.getOptions(),
-					mode	  : session.getMode().$id,
-					folds	 : session.getAllFolds().map(function(fold) {
-						return {
-							start		: fold.start,
-							end			: fold.end,
-							placeholder : fold.placeholder
-						};
-					}),
-					scrollTop  : session.getScrollTop(),
-					scrollLeft : session.getScrollLeft(),
-					
-					undos : session.getUndoManager().$undoStack.slice(-25),
-					redos : session.getUndoManager().$redoStack.slice(-25)
-				  
-				}
-			});
-			
-		})(editor.getSession()));
-	} catch (err) {
-		cb(err);
-	}
-}
+    
+        try {
+               cb (undefined,(function(session,state){
+        
+                if (state.editor.theme) {
+                    editor.setTheme("ace/theme/"+state.editor.theme);
+                }
+                
+                editor.setSession(sessionFromJSON(state.session));
 
+                
+            })(editor.getSession(),JSON.parse(json)));
+            
+        } catch (err) {
+            cb(err);
+        }
+    }
+    
+    window.ace_session_json = {
+        serialize   : serialize,
+        deserialize : deserialize
+     };
 
-function deserialize (editor,json,cb) {
-	
-	var Range = window.ace.require("ace/range").Range;
-	
-	function deltaRangeFixup (delta) {
-		delta.range = Range.fromPoints(delta.range.start, delta.range.end);
-	}
-	
-	function deltaArrayFixup(element) {
-		element.deltas.forEach(deltaRangeFixup);
-	}
-	
-	function stackElementFixup(stackElement) {
-		stackElement.forEach(deltaArrayFixup);
-	}
-	
-	function stackArrayFixup(stackArray) {
-		if (!stackArray) return [];
-		stackArray.forEach(stackElementFixup);
-		return stackArray;
-	}
-
-	try {
-		   cb (undefined,(function(session,state){
-	
-			editor.setTheme("ace/theme/"+state.editor.theme);
-			
-			session.setValue(state.session.value);
-			session.selection.fromJSON(state.session.selection);
-			session.setOptions(state.session.options);
-			session.setMode(state.session.mode);
-			try {
-				state.session.folds.forEach(function(fold){
-					session.addFold(fold.placeholder, 
-						Range.fromPoints(fold.start, fold.end));
-				});
-			} catch(e) {
-			}
-			session.setScrollTop(state.session.scrollTop);
-			session.setScrollTop(state.session.scrollLeft);
-			
-			var undoManager = session.getUndoManager();
-			
-			undoManager.$doc = session;
-			undoManager.$undoStack = stackArrayFixup(state.session.undos);
-			undoManager.$redoStack = stackArrayFixup(state.session.redos);
-			
-		})(editor.getSession(),JSON.parse(json)));
-		
-	} catch (err) {
-		cb(err);
-	}
-}
-  
+},["child.html"]);
